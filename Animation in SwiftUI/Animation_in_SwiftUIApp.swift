5289d07422642ff0831e74225ac2d64aca19cefb
AmrFiqi <amr.h.elfiqi@gmail.com>
2025-01-30 21:55:27 +0200

    Initial Commit

---
+++ Animation in SwiftUI/Animation_in_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animation_in_SwiftUIApp.swift
+//  Animation in SwiftUI
+//
+//  Created by Amr El-Fiqi on 30/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct Animation_in_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
